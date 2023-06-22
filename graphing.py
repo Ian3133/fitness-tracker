a874ef995d4e6a0545327298396b72d6c40441dd
Ian <iant@bu.edu>
2023-06-22 10:13:06 -0400

    base graphing done

--- graphing.py
+++ graphing.py
@@ -1,50 +1,48 @@
-
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime, timedelta
-from main import rowing, csv_data
-
-#print(csv_data[0])
-#print(str(datetime.now().date())
-#print(csv_data[1][1])
-#print(datetime(2023,1,4))
-#print(rowing[0][1][8:10])
-#conv = datetime(int(rowing[0][1][:4]), int(rowing[0][1][5:7]), int(rowing[0][1][8:10]))
-#print(datetime.today() < conv)
+from main import rowing, csv_data, running, cycling
 
 
-def show_all_activites(data, days_back):
+def show_all_activites(all_data, days_back):
     today = datetime.now().date()
     start_date = today - timedelta(days=days_back)  
     current_date = start_date
     
     all_days = []  
-    while current_date <= today:              # creates a list of all the dates back
-        all_days.append(current_date)# change this so not include year
+    while current_date <= today:            
+        all_days.append(current_date)# change this so not include year   .strftime("%m-%d")
         current_date += timedelta(days=1)
 
+    #activites = traverse_back(data, start_date) 
+    all_values = calc_values(all_data, all_days, days_back, start_date)
+    plt.bar(all_days, all_values, width = 1, color = "blue") # rowing and other counted here
+    run_vals = calc_values(running, all_days, days_back,start_date)
+    plt.bar(all_days, run_vals, width = 1, color = "red") # showing running
+    cycle_bars = calc_values(cycling, all_days, days_back,start_date)
+    plt.bar(all_days, cycle_bars, width = 1, color = "green") # showing cycling
+    # can add others and make clearer but find demo 
+    #others like streching and weights
 
-    #plt.bar(all_days, [0]*len(all_days), align="center")
-    #print(all_days)
-    activites = traverse_back(data, start_date) 
-    print(activites)
-       
+
+    plt.xlabel("Date")
+    plt.ylabel("activities")
+    plt.title("All Training")
+    plt.show()
+
+def calc_values(data, all_days, days_back, start_date):
+    activites = traverse_back(data, start_date)
     values = [0]*len(all_days)
     count = activites     -2 
     for i in range(days_back):
         oldest_act = datetime(int(data[count][1][:4]), int(data[count][1][5:7]), int(data[count][1][8:10])).date() 
-        if all_days[i] == oldest_act: # need to make this a loop for multiple activies on the same day
+        if all_days[i] == oldest_act: 
             values[i] += 1
             count -= 1
-    print(values)
-    
-        
-    plt.bar(all_days, values, align="center")
-    plt.xlabel("x-label")
-    plt.ylabel("y-label")
-    plt.title("title here")
-    plt.show()
-
+            while all_days[i] == datetime(int(data[count-1][1][:4]), int(data[count-1][1][5:7]), int(data[count-1][1][8:10])).date():
+                values[i] += 1
+                count -= 1
+    return values
 
 def traverse_back(data, start_date):
     """find how far back till we reach the date given returns the number, including activites within the start date 
@@ -60,55 +58,6 @@ def traverse_back(data, start_date):
             date = datetime(int(data[count][1][:4]), int(data[count][1][5:7]), int(data[count][1][8:10])).date()
             count+= 1
     return(count)
-#example call for above
-#ex_val = datetime(2023, 6, 12)
-#print(traverse_back(rowing, ex_val))
 
+show_all_activites(csv_data[1:], 15)
 
-
-show_all_activites(csv_data[1:], 24)
-print(csv_data[1:20])
-
-'''
-#plt.plot(rowing[1],rowing[4])
-# Add labels and title
-##plt.xlabel('X-axis')
-#plt.ylabel('Y-axis')
-#plt.title('Rows')
-# Display the graph
-#plt.show()
-#plt.savefig('plot.png') for turning into a png where a website 
-
-
-dates = [
-    datetime(2023, 1, 1),
-    datetime(2023, 2, 1),
-    datetime(2023, 3, 1),
-    datetime(2023, 4, 1),
-    datetime(2023, 5, 1)
-]
-
-values = [5, 10, 8, 12, 6]
-
-# Convert dates to matplotlib format
-dates_num = mdates.date2num(dates)
-
-# Create scatter plot
-plt.scatter(dates_num, values)
-
-# Format x-axis as dates
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-
-# Rotate x-axis labels for better visibility
-#plt.xticks(rotation=45)
-
-# Add labels and title
-plt.xlabel('Date')
-plt.ylabel('Value')
-plt.title('Scatter Plot')
-
-# Display the plot
-plt.show()
-
-
-'''
