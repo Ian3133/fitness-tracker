57785cb7f15dcf9aac56c1e3119d63a59f4cbd72
Ian <iant@bu.edu>
2023-06-25 17:07:21 -0400

    leaveing for thing  jsut ot be safe

--- scratch.py
+++ scratch.py
@@ -1,23 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime, timedelta
 
-# Generate data for x-axis
-x = np.linspace(1, 10, 100)  # Adjust the range and number of points as needed
 
-# Generate data for y-axis (logarithmic values)
-y = np.log10(x)  # Change the base or function as desired (e.g., np.log(), np.log2())
+# Example usage
+time_strings = ['0:00', '5:15', '2:00', '1:45', '7:30']
+# Example usage
+plot_time(time_strings)
 
-# Create the line graph
-plt.plot(x, y, '-', label='this Function')  # Add label to the plot
 
-# Add a legend
-plt.legend()
+# class week():
+#     def __init__(self, data, geer):
+#         self.data = data
+#         self.ok = geer
+        
+#     def total_time(self, data):
+#         #will calc the time for all the data given
+        
+#     def 
+        
 
-plt.xlabel('X')
-plt.ylabel('Logarithmic Y')
-plt.title('Logarithmic Function')
-plt.grid(True)
+# # Generate data for x-axis
+# x = np.linspace(1, 10, 100)  # Adjust the range and number of points as needed
 
-# Display the graph
-plt.tight_layout()
-plt.show()
+# # Generate data for y-axis (logarithmic values)
+# y = np.log10(x)  # Change the base or function as desired (e.g., np.log(), np.log2())
+
+# # Create the line graph
+# plt.plot(x, y, '-', label='this Function')  # Add label to the plot
+
+# # Add a legend
+# plt.legend()
+
+# plt.xlabel('X')
+# plt.ylabel('Logarithmic Y')
+# plt.title('Logarithmic Function')
+# plt.grid(True)
+
+# # Display the graph
+# plt.tight_layout()
+# plt.show()
