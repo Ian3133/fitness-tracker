8604d25fbbd1714bd501dbcbdf2a1a13855ac61d
Ian <iant@bu.edu>
2023-06-27 14:21:02 -0400

    reader and streching editing

--- week.py
+++ week.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 from datetime import datetime, timedelta
-from main import rowing, csv_data, running, cycling, other
-data = csv_data[1:]
+from main import data, streching, running, cycling, weights, rowing, other
+
 
 class week():
     def __init__(self, monday):
@@ -21,10 +21,16 @@ class week():
         self.all_times,self.all_hr = calc_time_and_hr(start_date, data, self.the_week)         
         run_val, run_hr= calc_time_and_hr(start_date, running, self.the_week)
         cycling_val, cycling_hr =  calc_time_and_hr(start_date, cycling, self.the_week)
-        #other_val = calc_time(start_date, other, the_week)             # need to add other as well as streching maybe auto do so it adds 10 mins if ever recored
-        #streching = 
-        self.all_times_min, self.run_min, self.cycling_min = to_min(self.all_times), to_min(run_val), to_min(cycling_val)    
-        print(run_val)
+        weights_val, extra = calc_time_and_hr(start_date, weights, self.the_week)
+        #other_val, extra = calc_time_and_hr(start_date, other, self.the_week)            
+        strech_val, extra = calc_time_and_hr(start_date, streching, self.the_week)
+        print(strech_val)
+        self.all_times_min, self.run_min, self.cycling_min = to_min(self.all_times), to_min(run_val), to_min(cycling_val)
+        self.weights_min = to_min(weights_val) # self.other_min = to_min(other_val)
+        self.strech_min =  calc_time_and_hr(start_date, streching, self.the_week)    # maybe double check this so not every day and actlly add to total times or change in data 
+        
+        
+        
         
     def show_week(self):
               
@@ -37,7 +43,10 @@ class week():
         plt.bar(times_graph_format, self.all_times_min, zorder= 2, color = "blue", label="Rowing")
         plt.bar(times_graph_format, self.run_min, zorder= 2, color = "red", label="Running") 
         plt.bar(times_graph_format, self.cycling_min, zorder= 2, color = "green", label="Cycling")
-        # add streching and other and probly weights
+        plt.bar(times_graph_format, self.weights_min, zorder= 2, color = "purple", label="Weights")
+        #plt.bar(times_graph_format, self.other_min, zorder= 2, color = "yellow", label="Other Exersize")
+        plt.bar(times_graph_format, self.strech_min, zorder= 2, color = "orange", label="Streching")
+ 
         plt.legend()
         plt.xlabel("Date")
         plt.ylabel("Minutes")
@@ -114,17 +123,18 @@ def calc_time_and_hr(monday, data, the_week):
     index_of_m = traverse_back(monday, data, len(data)) - 2
     times = ["0:00"]*7 
     HRs = []
-    for i in range(7):
-        oldest_act = datetime(int(data[index_of_m][1][:4]), int(data[index_of_m][1][5:7]), int(data[index_of_m][1][8:10])).date() 
-        if the_week[i] == oldest_act: 
-            times[i] = add_times(times[i], data[index_of_m][4][:-3])
-            HRs.append(int(data[index_of_m][5]))
-            index_of_m -= 1
-            while the_week[i] == datetime(int(data[index_of_m-1][1][:4]), int(data[index_of_m-1][1][5:7]), int(data[index_of_m-1][1][8:10])).date():
-                #deals with multiple workouts on the same day
-                times[i] = add_times(times[i], data[index_of_m][4][:-3])
+    if index_of_m > 0:
+        for i in range(7):
+            oldest_act = datetime(int(data[index_of_m][1][:4]), int(data[index_of_m][1][5:7]), int(data[index_of_m][1][8:10])).date() 
+            if the_week[i] == oldest_act: 
+                times[i] = add_times(times[i], str(data[index_of_m][4]).split(".")[0][:-3])
                 HRs.append(int(data[index_of_m][5]))
                 index_of_m -= 1
+                while the_week[i] == datetime(int(data[index_of_m-1][1][:4]), int(data[index_of_m-1][1][5:7]), int(data[index_of_m-1][1][8:10])).date():
+                    #deals with multiple workouts on the same day
+                    times[i] = add_times(times[i], str(data[index_of_m][4]).split(".")[0][:-3])
+                    HRs.append(int(data[index_of_m][5]))
+                    index_of_m -= 1       
     return times, HRs
 
 def traverse_back(monday, W_data, max_data):
@@ -154,12 +164,11 @@ def add_times(time1, time2):
 
     return result.strftime(format_str)
 
-
         
 # monday_1 = datetime(2023, 6, 5).date()
 # week_1 = week(monday_1)
 # week_1.show_week()
-monday_2 = datetime(2023, 6, 12).date()
+monday_2 = datetime(2023, 6, 23).date()
 week_2 = week(monday_2)
 week_2.show_week()
 #print(week_2.total_time())
